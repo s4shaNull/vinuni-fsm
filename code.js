e970dd9a9ce04b7b58eac5b0f68b69b7ed72d266
oreo01-hippiepham <hippiepham01@gmail.com>
2021-12-08 12:11:48 +0700

    code.js

---
+++ code.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	
+	$(".btn").click(function(){
+		
+		var tipoEstado = parseInt($("input[name='tipo']:checked").val());
+		var qtdEstados = parseInt($("option:selected").val());
+		var qtdEntradas = parseInt("1");
+		var qtdSaidas = parseInt("1");
+		
+		var tp = "Mealy";
+		if(tipoEstado == 1) tp = "Moore";
+		
+		qtdEstados++;
+		
+		var tabela = $("textarea[name='tabelaVerdade']").val();
+		var vetorTop = tabela.replace( /\n/g," ").split(" ");
+		for(var i = 0; i < vetorTop.length; i++){
+			vetorTop[i] = parseInt(vetorTop[i],10);
+		}
+		
+		localStorage.setItem("tipoMaquina",tipoEstado);
+		localStorage.setItem("qtdEst",qtdEstados);
+		localStorage.setItem("vetTop",vetorTop);
+		window.open("simulacao.html");
+		
+	});
+	
+});
